a6babec66a9f4e57744487ca886f956957d41c26
koenigigor <koenigigor@yandex.ru>
2023-04-10 23:28:48 +0300

    Initial commit

---
+++ Lantern.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lantern.h"
+
+#include "Components/PointLightComponent.h"
+#include "Components/SphereComponent.h"
+#include "Net/UnrealNetwork.h"
+
+
+ALantern::ALantern()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	Collision = CreateDefaultSubobject<USphereComponent>("Collision");
+	RootComponent = Collision;
+
+	PointLight = CreateDefaultSubobject<UPointLightComponent>("Light");
+	PointLight->SetupAttachment(Collision);
+}
+
+void ALantern::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, bDestroyed);
+	DOREPLIFETIME(ThisClass, bLightEnabled);
+}
+
+void ALantern::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	EnableLight_Internal(bEnabledByDefault);
+}
+
+bool ALantern::IsDestroyed() const
+{
+	return bDestroyed;
+}
+
+
+float ALantern::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
+	AActor* DamageCauser)
+{
+	const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (ActualDamage > 0.f) DestroyLantern();
+
+	return ActualDamage;
+}
+
+
+void ALantern::ToggleLight()
+{
+	if (HasAuthority())
+		EnableLight_Internal(!bLightEnabled);
+}
+
+void ALantern::EnableLight(bool bNowEnabled)
+{
+	if (HasAuthority())
+		EnableLight_Internal(bNowEnabled);
+}
+
+void ALantern::EnableLight_Internal(bool bNowEnabled)
+{
+	if (bNowEnabled && IsDestroyed()) return;
+
+	bLightEnabled = bNowEnabled;
+
+	//apply light parameters
+	const auto& Params = bNowEnabled ? EnabledParams : DisabledParams;
+	PointLight->SetLightColor(Params.Color);
+	PointLight->SetIntensity(Params.Intensity);
+
+	//send notify only if lantern live
+	if (!IsDestroyed())
+	{
+		bLightEnabled ? BP_OnLightEnabled() : BP_OnLightDisabled();
+		OnLanternToggleDelegate.Broadcast(bLightEnabled);
+	}
+}
+
+
+void ALantern::DestroyLantern()
+{
+	EnableLight(false);
+	SetCanBeDamaged(false);
+	bDestroyed = true;
+
+	BP_OnLanternDestroy();
+	OnLanternDestroyDelegate.Broadcast();
+}
+
+
+void ALantern::OnRep_Destroyed()
+{
+	DestroyLantern();
+}
+
+void ALantern::OnRep_Enabled()
+{
+	EnableLight_Internal(bLightEnabled);
+}
---
+++ Lantern.h
@@ -0,0 +1,95 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Lantern.generated.h"
+
+class UPointLightComponent;
+class USphereComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLanternDestroyDelegate);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLanternToggleDelegate, bool, bEnabled);
+
+USTRUCT(BlueprintType)
+struct FLanternParams
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FLinearColor Color = FLinearColor::White;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Intensity = 5000.f;
+};
+
+UCLASS()
+class TASKNETLAMP_API ALantern : public AActor
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	bool bEnabledByDefault = true;
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	FLanternParams EnabledParams = {FLinearColor::White, 5000.f};
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	FLanternParams DisabledParams = {FLinearColor::Red, 30.f};
+
+public:
+	ALantern();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDestroyed() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void ToggleLight();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void EnableLight(bool bNowEnabled);
+
+	UPROPERTY(BlueprintAssignable, Transient)
+	FOnLanternDestroyDelegate OnLanternDestroyDelegate;
+
+	UPROPERTY(BlueprintAssignable, Transient)
+	FOnLanternToggleDelegate OnLanternToggleDelegate;
+
+protected:
+	void EnableLight_Internal(bool bNowEnabled);
+
+	void DestroyLantern();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "OnLightEnabled")
+	void BP_OnLightEnabled();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "OnLightDisabled")
+	void BP_OnLightDisabled();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "OnLanternDestroy")
+	void BP_OnLanternDestroy();
+
+	UPROPERTY(ReplicatedUsing="OnRep_Destroyed")
+	bool bDestroyed = false;
+
+	UPROPERTY(ReplicatedUsing="OnRep_Enabled")
+	bool bLightEnabled = true;
+
+	UFUNCTION()
+	void OnRep_Destroyed();
+
+	UFUNCTION()
+	void OnRep_Enabled();
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USphereComponent* Collision = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPointLightComponent* PointLight = nullptr;
+};
